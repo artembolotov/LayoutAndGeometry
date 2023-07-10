bafa69751ddb5746b8536e23df27ae6e99aaf6a2
Artem Bolotov <teeemon@gmail.com>
2023-07-10 18:46:15 +0300

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by artembolotov on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
