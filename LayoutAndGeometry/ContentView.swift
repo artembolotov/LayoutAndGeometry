fa8a73d9e3c13e2c3910ed2c68cd62cc2df1ae85
Artem Bolotov <teeemon@gmail.com>
2023-07-12 08:04:38 +0300

    Custom Layout

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -7,15 +7,35 @@
 
 import SwiftUI
 
+extension VerticalAlignment {
+    enum  MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+    
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@artembolotov")
+                    .alignmentGuide(.midAccountAndName) { $0[VerticalAlignment.center] }
+                
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 64, height: 64)
+            }
+            
+            VStack {
+                Text("Full name:")
+                Text("ARTEM BOLOTOV")
+                    .alignmentGuide(.midAccountAndName) { $0[VerticalAlignment.center] }
+                    .font(.largeTitle)
+            }
         }
-        .padding()
     }
 }
 
